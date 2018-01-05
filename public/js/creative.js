643a7b9ed3c9ee6603b4480e712e834b593f9af0
Jannik Arndt <jannik@jannikarndt.de>
2018-01-05 07:45:14 +0100

    Release all changes since Nov 7

--- public/js/creative.js
+++ public/js/creative.js
@@ -8,7 +8,7 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll, aside.toc a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
